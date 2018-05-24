0b6b58d34edc22282c090bf37ef77d024a4483e7
Jordan Hayes <jhayes@ucr.edu>
2018-05-24 11:59:59 -0700

    Added missing code and removed extra files

--- R/modulelist.R
+++
@@ -1,5 +0,0 @@
-modulelist <-
-function() {
-  module("avail")
-  warning("The function modulelist will be deprecated in future releases, please refer to the documentation for proper useage.")
-}
--- R/moduleload.R
+++
@@ -1,5 +0,0 @@
-moduleload <-
-function(module,envir="PATH") {
-  module("load",module)
-  warning("The function moduleload will be deprecated in future releases, please refer to the documentation for proper useage.")
-}
